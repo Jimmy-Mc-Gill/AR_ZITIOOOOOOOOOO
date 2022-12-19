3eb0d6913b8ad20ebc0eff532b38525775c3c6af
Permatrago10 <114485628+Permatrago10@users.noreply.github.com>
2022-12-19 10:30:23 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//corre solo una vez cuando inicia el programa
+function setup(){
+    console.log("setup - frameCount:"+frameCount);
+    
+    createCanvas(900,900);
+    background(125);
+
+
+}
+
+function draw(){
+    console.log("draw - frameCount:"+frameCount);
+    fill(  228, 140, 241  )
+    triangle(775,625,450,150,125,625)
+    fill(  228, 140, 241  )
+    triangle(775,275,450,750,125,275)
+    fill(  180, 236, 247  );
+    ellipse(height/2,width/2,450,450)
+    fill(  191, 180, 247  )
+    square(270,280,360,20);
+    fill(  117, 126, 239  )
+    triangle(625,380,275,380,450,640)
+    fill(  117, 126, 239  )
+    triangle(625,540,275,540,450,280)
+    
+}
