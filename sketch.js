49e9a640e244fccbd1aaf6e9ec6db7d1e466d566
Permatrago10 <114485628+Permatrago10@users.noreply.github.com>
2022-12-20 13:04:10 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,6 +10,8 @@ function setup(){
 
 function draw(){
     console.log("draw - frameCount:"+frameCount);
+    fill(  191, 180, 247  )
+    ellipse(450,450,550,550)
     fill(  228, 140, 241  )
     triangle(775,625,450,150,125,625)
     fill(  228, 140, 241  )
@@ -22,5 +24,11 @@ function draw(){
     triangle(625,380,275,380,450,640)
     fill(  117, 126, 239  )
     triangle(625,540,275,540,450,280)
+    arc(235,220,80,80,90,270, PIE);
+    arc(665,220,80,80,15,315, PIE);
+    arc(235,680,80,80,0,180, PIE);
+    arc(665,680,80,80,225,135, PIE);
     
+
+
 }
